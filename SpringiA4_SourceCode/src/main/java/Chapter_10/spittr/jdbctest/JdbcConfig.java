e9b1e531371edbdeac003ef69532e459c4046394
xiaobai-cyn <599283258@qq.com>
2018-03-29 22:13:33 +0800

    jdbctemplate

---
+++ SpringiA4_SourceCode/src/main/java/Chapter_10/spittr/jdbctest/JdbcConfig.java
@@ -0,0 +1,48 @@
+package Chapter_10.spittr.jdbctest;
+
+import javax.sql.DataSource;
+
+import Chapter_10.spittr.db.SpitterRepository;
+import Chapter_10.spittr.db.SpittleRepository;
+import Chapter_10.spittr.db.jdbc.JdbcSpitterRepository;
+import Chapter_10.spittr.db.jdbc.JdbcSpittleRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@Configuration
+public class JdbcConfig {
+
+  @Bean
+  public DataSource dataSource() {
+    return new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.H2)
+      .addScripts("classpath:schema.sql", "classpath:test-data.sql")
+      .build();
+  }
+  
+  @Bean
+  public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+    return new JdbcTemplate(dataSource);
+  }
+  
+  @Bean
+  public SpitterRepository spitterRepository(JdbcTemplate jdbcTemplate) {
+    return new JdbcSpitterRepository(jdbcTemplate);
+  }
+
+  @Bean
+  public SpittleRepository spittleRepository(JdbcTemplate jdbcTemplate) {
+    return new JdbcSpittleRepository(jdbcTemplate);
+  }
+  
+  @Bean
+  public PlatformTransactionManager transactionManager(DataSource dataSource) {
+    return new DataSourceTransactionManager(dataSource);
+  }
+
+}
---
+++ SpringiA4_SourceCode/src/test/java/configurationtest/TestMain.java
@@ -0,0 +1,16 @@
+package configurationtest;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class TestMain {
+    public static void main(String[] args) {
+
+        // @Configuration注解的spring容器加载方式，用AnnotationConfigApplicationContext替换ClassPathXmlApplicationContext
+        ApplicationContext context = new AnnotationConfigApplicationContext(TestConfiguration.class);
+
+        // 如果加载spring-context.xml文件：
+        // ApplicationContext context = new
+        // ClassPathXmlApplicationContext("spring-context.xml");
+    }
+}
