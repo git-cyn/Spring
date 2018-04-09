b5775de54054dab2e006fb8efee9b7d43cfa6a3b
xiaobai-cyn <599283258@qq.com>
2018-04-09 21:17:57 +0800

    修改config数据库连接池。

--- SpringiA4_SourceCode/src/main/java/s_10/spittr/jdbctest/JdbcConfig.java
+++ SpringiA4_SourceCode/src/main/java/s_10/spittr/jdbctest/JdbcConfig.java
@@ -1,13 +1,10 @@
 package s_10.spittr.jdbctest;
 
-import com.mchange.v2.c3p0.ComboPooledDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.transaction.PlatformTransactionManager;
 import s_10.spittr.db.SpitterRepository;
 import s_10.spittr.db.SpittleRepository;
--- SpringiA4_SourceCode/src/main/java/s_10/spittr/jdbctest/JdbcSpitterRepositoryTest.java
+++ SpringiA4_SourceCode/src/main/java/s_10/spittr/jdbctest/JdbcSpitterRepositoryTest.java
@@ -5,7 +5,6 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.transaction.annotation.Transactional;
 import s_10.spittr.db.jdbc.JdbcSpitterRepository;
 import s_10.spittr.domain.Spitter;
 
